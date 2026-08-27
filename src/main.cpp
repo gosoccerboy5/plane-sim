74da675281c2850e04f75e1011ea540b91104f3e
gosoccerboy5 <gosoccerboy6@gmail.com>
2026-08-27 14:38:34 -0700

    it works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,188 @@
-/*******************************************************************************************
-*
-*   raylib [models] example - yaw pitch roll
-*
-*   Example complexity rating: [★★☆☆] 2/4
-*
-*   Example originally created with raylib 1.8, last time updated with raylib 4.0
-*
-*   Example contributed by Berni (@Berni8k) and reviewed by Ramon Santamaria (@raysan5)
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2017-2025 Berni (@Berni8k) and Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include "raymath.h"
+#include "rlgl.h"
+#include "rcamera.h"
 
-#include "raymath.h"        // Required for: MatrixRotateXYZ()
+#include "physics.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+#include <random>
+#include <iostream>
+
+//`CameraMode` is already defined in raylib but not quite what we want
+enum CameraType {
+    THIRD_PERSON,
+    THIRD_PERSON_LOCKED,
+    FIRST_PERSON,
+};
+
+float random() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    
+    static std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+    
+    return dis(gen);
+}
+
+int main() {
+    const int screenWidth = 1600;
+    const int screenHeight = 900;
 
     //SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_HIGHDPI);
-    InitWindow(screenWidth, screenHeight, "raylib [models] example - yaw pitch roll");
+    InitWindow(screenWidth, screenHeight, "Raylib Plane Sim");
+    rlDisableBackfaceCulling();
+
 
     Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 50.0f, -120.0f };// Camera position perspective
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 30.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera type
-
-    Model model = LoadModel("src/assets/plane.obj");                  // Load model
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    Vector2 camAngle {0, 0};
+    CameraType cameraMode = CameraType::THIRD_PERSON;
     
+    float mouseWheelMomentum = 0;
 
-    float pitch = 0.0f;
-    float roll = 0.0f;
-    float yaw = 0.0f;
+    Model model = LoadModel("src/assets/plane.obj");
+    Model map = LoadModel("src/assets/landscape.obj"); 
+    Model skybox = LoadModel("src/assets/skybox.obj");
+    
+    Plane plane(&model);
+    
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    while (!WindowShouldClose()) {
+        Vector2 mouseMovement = GetMouseDelta();
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+            camAngle = camAngle + Vector2Scale(mouseMovement, 0.01);
+        }
+        camAngle.y = Clamp(camAngle.y, -3.14/2, 3.14/2);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // Plane pitch (x-axis) controls
-        if (IsKeyDown(KEY_DOWN)) pitch += 0.6f;
-        else if (IsKeyDown(KEY_UP)) pitch -= 0.6f;
-        else
-        {
-            if (pitch > 0.3f) pitch -= 0.3f;
-            else if (pitch < -0.3f) pitch += 0.3f;
+        mouseWheelMomentum += GetMouseWheelMove() * 0.02;
+        camera.fovy = Clamp(camera.fovy * (1-mouseWheelMomentum), 10, 120);
+        mouseWheelMomentum *= 0.9;
+        
+        
+        bool pitching = false, rolling = false;
+        
+        if (IsKeyDown(KEY_A)) {
+            plane.yaw(0.01);
+        }
+        if (IsKeyDown(KEY_D)) {
+            plane.yaw(-0.01);
+        }
+        float elevatorPitchRate = 0.01;
+        if (IsKeyDown(KEY_S)) {
+            plane.elevatorDeflection += elevatorPitchRate;
+            pitching = true;
+        }
+        if (IsKeyDown(KEY_W)) {
+            plane.elevatorDeflection -= elevatorPitchRate;
+            pitching = true;
+        }
+        if (!pitching) {
+            if (std::abs(plane.elevatorDeflection) < elevatorPitchRate) plane.elevatorDeflection = 0;
+            else if (plane.elevatorDeflection < 0) plane.elevatorDeflection += elevatorPitchRate;
+            else plane.elevatorDeflection -= elevatorPitchRate;
+        }
+        plane.elevatorDeflection = Clamp(plane.elevatorDeflection, -DEG2RAD*20, DEG2RAD*20);
+        
+        float aileronPitchRate = 0.02;
+        if (IsKeyDown(KEY_E)) {
+            plane.aileronDeflection += aileronPitchRate;
+            rolling = true;
+        }
+        if (IsKeyDown(KEY_Q)) {
+            plane.aileronDeflection -= aileronPitchRate;
+            rolling = true;
+        } 
+        if (!rolling) {
+            if (std::abs(plane.aileronDeflection) < aileronPitchRate) plane.aileronDeflection = 0;
+            else if (plane.aileronDeflection < 0) plane.aileronDeflection += aileronPitchRate;
+            else plane.aileronDeflection -= aileronPitchRate;
+        }
+        plane.aileronDeflection = Clamp(plane.aileronDeflection, -DEG2RAD*20, DEG2RAD*20);
+        plane.update();
+        
+        
+        if (IsKeyPressed(KEY_C)) {
+            if (cameraMode == CameraType::THIRD_PERSON) {
+                //cameraMode = CameraType::THIRD_PERSON_LOCKED;
+                cameraMode = CameraType::FIRST_PERSON;
+                camAngle = {0, 0};
+            } else if (cameraMode == CameraType::THIRD_PERSON_LOCKED) {
+                cameraMode = CameraType::FIRST_PERSON;
+                camAngle = {0, 0};
+            } else if (cameraMode == CameraType::FIRST_PERSON) {
+                cameraMode = CameraType::THIRD_PERSON;
+            }
+        }
+    
+        model.transform = {
+            plane.right().x, plane.up.x, plane.front.x, 0.0f,
+            plane.right().y, plane.up.y, plane.front.y, 0.0f,
+            plane.right().z, plane.up.z, plane.front.z, 0.0f,
+            0.0f,            0.0f,       0.0f,          1.0f,
+        };
+        //transform matrix can be formed directly from orientation vectors as columns (orientation vectors form x, y, z axes)
+        //as opposed to using MatrixRotateXYZ from euler angles (suffers from gimbal lock)
+        
+        float cameraDistance = 10;
+        
+        if (cameraMode == CameraType::THIRD_PERSON) {
+            camera.target = plane.position;
+            camera.up = {0, 1.0f, 0};
+            camera.position = Vector3Add(plane.position, Vector3Scale({sin(-camAngle.x)*cos(-camAngle.y), sin(-camAngle.y), cos(-camAngle.x)*cos(-camAngle.y)}, -cameraDistance));
+        } else if (cameraMode == CameraType::THIRD_PERSON_LOCKED) {
+            camera.target = plane.position;
+            camera.position = plane.position + Vector3Scale(
+                Vector3Scale(plane.right(), sin(camAngle.x)*cos(-camAngle.y)) + 
+                Vector3Scale(plane.up, sin(-camAngle.y)) + 
+                Vector3Scale(plane.front, cos(camAngle.x)*cos(-camAngle.y)), 
+                -cameraDistance);
+            camera.up = plane.up;
+        } else if (cameraMode == CameraType::FIRST_PERSON) {
+            camAngle.y = Clamp(camAngle.y, -1.5, 1);
+            camera.up = plane.up;
+            camera.position = plane.position + Vector3Scale(plane.up, 1) + Vector3Scale(plane.front, .5);
+            camera.target = camera.position + Vector3Scale(
+                Vector3Scale(plane.right(), sin(camAngle.x)*cos(-camAngle.y)) + 
+                Vector3Scale(plane.up, sin(-camAngle.y)) + 
+                Vector3Scale(plane.front, cos(camAngle.x)*cos(-camAngle.y)), 
+                10);
+        }
+        if (plane.elevatorDeflection != 0) {
+            Vector3 offset = Vector3Scale({random(), random(), random()}, plane.elevatorDeflection * 0.1);
+            if (cameraMode == CameraType::FIRST_PERSON) camera.position = camera.position + offset;
+            else if (cameraMode == CameraType::THIRD_PERSON || cameraMode == CameraType::THIRD_PERSON_LOCKED) plane.position = plane.position + offset;
         }
 
-        // Plane yaw (y-axis) controls
-        if (IsKeyDown(KEY_S)) yaw -= 1.0f;
-        else if (IsKeyDown(KEY_A)) yaw += 1.0f;
-        else
-        {
-            if (yaw > 0.0f) yaw -= 0.5f;
-            else if (yaw < 0.0f) yaw += 0.5f;
-        }
 
-        // Plane roll (z-axis) controls
-        if (IsKeyDown(KEY_LEFT)) roll -= 1.0f;
-        else if (IsKeyDown(KEY_RIGHT)) roll += 1.0f;
-        else
-        {
-            if (roll > 0.0f) roll -= 0.5f;
-            else if (roll < 0.0f) roll += 0.5f;
-        }
 
-        // Tranformation matrix for rotations
-        model.transform = MatrixRotateXYZ((Vector3){ DEG2RAD*pitch, DEG2RAD*yaw, DEG2RAD*roll });
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            // Draw 3D model (recomended to draw 3D always before 2D)
             BeginMode3D(camera);
+                rlDisableDepthMask();
+                DrawModel(skybox, plane.position, 2.0f, WHITE);
+                rlEnableDepthMask();
 
-                DrawModel(model, (Vector3){ 0.0f, -8.0f, 0.0f }, 1.0f, WHITE);   // Draw 3d model with texture
-                DrawGrid(10, 10.0f);
-
+                DrawModel(*plane.model, plane.position, 1.0f, WHITE);
+                
+                DrawModel(map, {0, 0, 0}, 1.0f, WHITE);
             EndMode3D();
 
-            // Draw controls info
-            DrawRectangle(30, 370, 260, 70, Fade(GREEN, 0.5f));
-            DrawRectangleLines(30, 370, 260, 70, Fade(DARKGREEN, 0.5f));
-            DrawText("Pitch controlled with: KEY_UP / KEY_DOWN", 40, 380, 10, DARKGRAY);
-            DrawText("Roll controlled with: KEY_LEFT / KEY_RIGHT", 40, 400, 10, DARKGRAY);
-            DrawText("Yaw controlled with: KEY_A / KEY_S", 40, 420, 10, DARKGRAY);
-
-            DrawText("(c) WWI Plane Model created by GiaHanLam", screenWidth - 240, screenHeight - 20, 10, DARKGRAY);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
+    
+    
+    UnloadModel(model);
+    UnloadModel(map);
+    UnloadModel(skybox);
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadModel(model);     // Unload model data
-
-    CloseWindow();          // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    CloseWindow();
+    
     return 0;
 }
--- src/physics.h
+++ src/physics.h
@@ -4,17 +4,59 @@
 #include "raylib.h"
 #include "raymath.h"
 
+Vector3 rotateVectorAroundAxis(const Vector3& vector, Vector3 axis, double radians) {
+    axis = Vector3Normalize(axis);
+    double theta = Vector3Angle(axis, vector);
+    double radius = Vector3Length(vector);
+    Vector3 vertical = Vector3Scale(axis, radius * cos(theta));
+    Vector3 flat = vector - vertical;
+    Vector3 flatPerpendicular = Vector3CrossProduct(axis, flat);
+    Vector3 resultant = Vector3Scale(flat, cos(radians)) + Vector3Scale(flatPerpendicular, sin(radians)) + vertical;
+    return resultant;
+}
+
 class Plane {
     public:
     Vector3 position;
     Vector3 velocity;
-    Vector3 front;
-    Vector3 up;
+    Vector3 front {0, 0, 1};
+    Vector3 up {0, 1, 0};
+    Model* model;
+    float elevatorDeflection = 0;
+    float aileronDeflection = 0; 
     
+    Plane(Model* model): model(model) {
+        velocity = {0, 0, 5};
+        position = (Vector3){ 0.0f, 20.0f, 0.0f };
+    }
+    
+    void normalizeOrientationVectors() {
+        front = Vector3Normalize(front);
+        up = Vector3Normalize(up);
+    }
     void update() {
+        velocity = Vector3Scale(front, Vector3Length(velocity));
         position = position + velocity;
+        pitch(elevatorDeflection * 0.03);
+        roll(aileronDeflection * .2);
+    }
+    Vector3 right() const {
+        return Vector3CrossProduct(front, up);
+    }
+    void roll(double angle) {
+        up = rotateVectorAroundAxis(up, front, angle);
+        normalizeOrientationVectors();
+    }
+    void pitch(double angle) {
+        Vector3 rightVector = right();
+        front = rotateVectorAroundAxis(front, rightVector, angle);
+        up = rotateVectorAroundAxis(up, rightVector, angle);
+        normalizeOrientationVectors();
+    }
+    void yaw(double angle) {
+        front = rotateVectorAroundAxis(front, up, angle);
+        normalizeOrientationVectors();
     }
 };
-
 
 #endif
